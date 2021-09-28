48f082a9a2d4ff2ea0c723c3add604bf2ec97067
Christine15Mnl <christinemanalu@student.telkomuniversity.ac.id>
2021-09-28 11:34:38 +0700

    HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld
+
+	public static void main(String[] args) {
+		System.out.println("Nama  : Christine");
+		System.out.println("NIM   : 6706213003");
+		System.out.println("Kelas : D3IF4501");
+	}
+
+}
